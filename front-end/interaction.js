2de1c3df0c717816fd2e98defa0b324131b48962
Сергей Ятченко <ssya20031127@gmail.com>
2024-05-27 14:38:33 +0300

    Rampart, Stronghold, Conflux and Inferno artwork update

--- front-end/interaction.js
+++ front-end/interaction.js
@@ -32,7 +32,7 @@ const baseUrl = `http://localhost:3000/`
 
 // }
 var currentURL = window.location.href;
-const pattern = /end\/(\w+)/;
+const pattern = /0\/(\w+)/;
 const match = currentURL.match(pattern);
 const extractedWord = match[1];
 console.log("Extracted word:", extractedWord);
